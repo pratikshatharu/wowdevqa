669c979d19d10707300d0750861ff36b7e1cc9fc
pratikshatharu <pratikshachaudhary728@gmail.com>
2021-08-31 00:24:54 +0545

    services added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,7 +7,17 @@ function Body() {
         <>
             <Topbar />
             <section>
-                <h3 className="text-white"><u>SoftwareDevelopment:Testing:Quality:Assurance:Nepal</u></h3>
+                <h3 className="text-primary"><u>SoftwareDevelopment:Testing:Quality:Assurance:Nepal</u></h3>
+                <h1 className="text-white">Our Services</h1>
+                <ol className="text-white">
+                    <li typeof="number">ML:Machine Learning</li>
+                    <li typeof="number">DP:DeepLearning</li>
+                    <li typeof="number">AI:Artificial Intelligence</li>
+                    <li typeof="number">QA:Quality Asurance</li>
+                    <li typeof="number">CS:Cyber Security</li>
+                    <li typeof="number">Hacking</li>
+                    <li typeof="number">Security Testing</li>
+                </ol>
             </section>
 
         </>
