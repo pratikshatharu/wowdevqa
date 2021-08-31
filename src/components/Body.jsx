758e9b6c13f653aab6b0d74b606524c47522e451
pratikshatharu <pratikshachaudhary728@gmail.com>
2021-08-31 08:09:43 +0545

    contact and email added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./body.css";
-import Topbar from './Navbar'
+import Topbar from './Navbar';
+import { Email, Phone } from '@material-ui/icons';
 
 function Body() {
     return (
@@ -8,16 +9,13 @@ function Body() {
             <Topbar />
             <section>
                 <h3 className="text-primary"><u>SoftwareDevelopment:Testing:Quality:Assurance:Nepal</u></h3>
-                <h1 className="text-white">Our Services</h1>
-                <ol className="text-white">
-                    <li typeof="number">ML:Machine Learning</li>
-                    <li typeof="number">DP:DeepLearning</li>
-                    <li typeof="number">AI:Artificial Intelligence</li>
-                    <li typeof="number">QA:Quality Asurance</li>
-                    <li typeof="number">CS:Cyber Security</li>
-                    <li typeof="number">Hacking</li>
-                    <li typeof="number">Security Testing</li>
-                </ol>
+
+                <ul className="text-white">
+                    <li ><h5>ML:Machine Learning,DP:DeepLearning,AI:Artificial,AI:Artificial Intelligence </h5></li>
+                    <li ><h5>QA:Quality Asurance,CS:Cyber Security,Hacking,Security Testing ,Kalilinux</h5></li>
+
+                </ul>
+                <h6>{<Phone />} Contact:+9779841224387,+9779801034557 {<Email />} Email:wowdevqa@wowdevqa.com,lavakafle@wowdevqa.com</h6>
             </section>
 
         </>
